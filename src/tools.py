70277126072459106ed19e29d8f33ebeb11a4a2a
sarvin <scoachbuilder@dealersocket.com>
2022-07-27 19:55:38 +0000

    requests session timeout added: Later we'll make this configurable.

--- src/tools.py
+++ src/tools.py
@@ -1,10 +1,13 @@
 """Module holding various helper classes"""
-from typing import NamedTuple, TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import requests
 
-class Connection(NamedTuple):
+@dataclass
+class Connection():
     """Store request session and base url in simple object"""
     session: 'requests.sessions.Session'
     base_url: str
+    session_timeout: int = 15 #TODO Pass this value in to allow user configuration
